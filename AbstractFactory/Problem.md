809d86b982b1e0a972109f6fbfcb11225b444ab5
CuongTruong <truongquoccuongbt@gmail.com>
2024-06-20 22:07:24 +0700

    commit

--- AbstractFactory/Problem.md
+++ AbstractFactory/Problem.md
@@ -1,27 +1,11 @@
 ### Problem
-Program:
-    + Math
-    + Literature
-    + English
 
-We have 3 region at this time:
-+ V1
-+ v2
-+ V3
+Event marketing of Coconut Technology \
+have 2 event classic and bussiness
 
-Each Region have difference Price
++ Mobile Phone
++ Tablet
++ Watch
 
-+ V1:
-    - Math: 1000
-    - Literature: 1000
-    - English: 1000
 
-+ V2:
-    - Math: 850
-    - Literature: 850
-    - English: 850
 
-+ V3:
-    - Math: 1500
-    - Literature: 1500
-    - English: 1500
--- AbstractFactory/before.py
+++ AbstractFactory/before.py
@@ -1,50 +1,143 @@
 
-
-
-
-
-
-class Program:
-    def __init__(self, name: str, price: float, rate: float) -> None:
+class Mobile:
+    def __init__(self, name: str, price: float, code: str) -> None:
         self.name: str = name
         self.price: float = price
-        self.rate: float = rate
+        self.code: str = code
     
-    def get_price(self):
-        total: float = self.price + (self.price * self.rate)
-        print(f'Price of program {self.name}: {total}')
+    def to_string(self) -> str:
+        return f'Model {self.code} - price: {self.price}'
 
-class Region:
-    def __init__(self, name: str, math: Program, liter: Program, eng: Program) -> None:
+
+
+class BussinessMobile:
+    def __init__(self, name: str, price: float, code: str) -> None:
         self.name: str = name
-        self.math: Program = math
-        self.literature: Program = liter
-        self.english: Program = eng
+        self.price: float = price
+        self.code: str = code
+        self.dynamon: bool = True
+    
+    def to_string(self):
+        return f'Model {self.code} - price: {self.price}'
 
-    def print_region(self):
-        text: str = 'Region ' + self.name + '\n'
-        text += 'Math: ' + self.math.get_price()
-        text += 'Literature: ' + self.literature.get_price()
-        text += 'English: ' + self.english.get_price()
-        print(text)
+
+class BussinessTablet:
+    def __init__(self, name: str, screen_size: int, price: float) -> None:
+        self.name: str = name
+        self.screen_size = screen_size
+        self.price = price
+        self.dynamon: bool = True
+
+    def to_string(self):
+        text: str = 'Name ' + self.name + '\n'
+        text += 'screen_size: ' + str(self.screen_size) + '\n'
+        text += 'Price: ' + str(self.price) + '\n'
+        return text
+
+
+class Tablet:
+    def __init__(self, name: str, screen_size: int, price: float) -> None:
+        self.name: str = name
+        self.screen_size = screen_size
+        self.price = price
+
+    def to_string(self):
+        text: str = 'Name ' + self.name + '\n'
+        text += 'screen_size: ' + str(self.screen_size) + '\n'
+        text += 'Price: ' + str(self.price) + '\n'
+        return text
+
+
+class BussinessWatch:
+    def __init__(self, name: str = None, type: str = None, sex: str = None) -> None:
+        self.name: str = name
+        self.type: str = type
+        self.sex: str = sex
+
+    def to_string(self) -> str:
+        return f'Name: {self.name} - type: {self.type} - sex: {self.sex}'
+
+
+class Watch:
+    def __init__(self, name: str = None, type: str = None, sex: str = None) -> None:
+        self.name: str = name
+        self.type: str = type
+        self.sex: str = sex
+        self.dynamon: bool = True
+
+    def to_string(self) -> str:
+        return f'Name: {self.name} - type: {self.type} - sex: {self.sex}'
+
+
+class ClassicProduct:
+    def __init__(self, name_type:str) -> None:
+        self.mobile = None
+        self.watch  = None
+        self.tablet = None
+        self.name_type = None
+
+    def add_mobile(self, mobile: Mobile) -> None:
+        self.mobile = mobile
+
+    def add_watch(self, watch: Watch) -> None:
+        self.watch = watch
+    
+    def add_tablet(self, tablet: Tablet) -> None:
+        self.tablet = tablet
+
+    def print(self) -> str:
+        print(self.mobile.to_string())
+        print(self.watch.to_string())
+        print(self.tablet.to_string())
+     
+
+class BussinessProduct:
+    def __init__(self, name_type:str) -> None:
+        self.mobile: Mobile = None
+        self.watch: Watch  = None
+        self.tablet: Tablet = None
+        self.name_type: str = None
+
+    def add_mobile(self, mobile: BussinessMobile) -> None:
+        self.mobile = mobile
+
+    def add_watch(self, watch: BussinessWatch) -> None:
+        self.watch = watch
+    
+    def add_tablet(self, tablet: BussinessTablet) -> None:
+        self.tablet = tablet
+
+    def print(self) -> str:
+        print(self.mobile.to_string())
+        print(self.watch.to_string())
+        print(self.tablet.to_string())
 
 
 def main():
-    math_v1: Program = Program('Math v1', 1000, 1)
-    math_v2: Program = Program('Math v2', 1000, -0.5)
-    math_v3: Program = Program('Math v3', 1000, 0.5)
+    type_class: str = 'buss'
 
-    literature_v1: Program = Program('literature v1', 1000, 1)
-    literature_v2: Program = Program('literature v2', 1000, -0.5)
-    literature_v3: Program = Program('literature v3', 1000, 0.5)
+    mobile_classic: Mobile = Mobile('apple', 1000, '0001')
+    watch_classic: Watch = Watch('galaxy watch', 1000, 'Female')
+    tablet_classic: Tablet = Tablet('ipad mini', 15, 5000)
 
-    english_v1: Program = Program('english v1', 1000, 1)
-    english_v2: Program = Program('english v2', 1000, -0.5)
-    english_v3: Program = Program('english v3', 1000, 0.5)
+    if(type_class == 'class'):
+        classic_product: ClassicProduct = ClassicProduct(name_type='classic')
 
-    reg_v1:Region = Region(name='v1')
-    reg_v2:Region = Region(name='v2')
-    reg_v3:Region = Region(name='v3')
+        classic_product.add_mobile(mobile_classic)
+        classic_product.add_watch(watch_classic)
+        classic_product.add_tablet(tablet_classic)
+
+        classic_product.print()
+    elif (type_class == 'buss'):
+        mobile_bus: BussinessMobile = BussinessMobile('buss apple', 1000, '0001')
+        watch_bus: BussinessWatch = BussinessWatch('buss galaxy watch', 1000, 'Female')
+        tablet_bus: BussinessTablet = BussinessTablet('buss ipad mini', 15, 5000)
+
+        bussiness_product: BussinessProduct = BussinessProduct(name_type='bussiness')
+        bussiness_product.add_mobile(mobile_bus)
+        bussiness_product.add_tablet(tablet_bus)
+        bussiness_product.add_watch(tablet_bus)
+        bussiness_product.print()
 
 
 
